8159924feee464d47bfd6f02cf7f473fc931fb0b
tyVespA <migliavacca.marco08@gmail.com>
2023-11-05 12:41:03 +0100

    Finish Header

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,6 @@ import Header from "./components/Header";
 function App() {
   return (
     <>
-      <h1>hi</h1>
       <Header />
     </>
   );
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,17 @@
-import React from "react";
+import styles from "../styles/Header.module.css";
+import Logo from "../assets/images/logo.svg";
 
 function Header() {
-  return <div>Header</div>;
+  return (
+    <header className={styles.header}>
+      <img src={Logo} alt="logo" />
+      <h1>A history of everything you copy</h1>
+      <p>
+        Clipboard allows you to track and organize everything you copy.
+        Instantly access your clipboard on all your devices.
+      </p>
+    </header>
+  );
 }
 
 export default Header;
