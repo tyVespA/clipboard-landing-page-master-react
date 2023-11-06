a895f4fd4133db2e482dac92a6d94ed93a9950eb
tyVespA <migliavacca.marco08@gmail.com>
2023-11-06 01:57:05 +0100

    Add new components

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,32 @@
 import Header from "./components/Header";
+import styles from "./App.module.css";
+import SectionOne from "./components/SectionOne";
+import SectionTwo from "./components/SectionTwo";
+import SectionThree from "./components/SectionThree";
+import Partners from "./components/Partners";
+import Cta from "./components/Cta";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <>
       <Header />
+      <section className={styles.section}>
+        <SectionOne />
+      </section>
+      <section className={styles.section}>
+        <SectionTwo />
+      </section>
+      <section className={styles.section}>
+        <SectionThree />
+      </section>
+      <section className={styles.section}>
+        <Partners />
+      </section>
+      <section className={styles.section}>
+        <Cta />
+      </section>
+      <Footer />
     </>
   );
 }
